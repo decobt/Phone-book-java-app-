aeff4d9c8392fcc78ba97d1ec6b5b0b8e266a414
Trajche Roshkoski <roskoski_trajce@yahoo.com>
2012-11-11 23:38:34 +0100

    Glavna Forma
    Glavna forma bez komponenti

---
+++ TelefonskiImenik/src/Glavna.java
@@ -0,0 +1,13 @@
+
+public class Glavna {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Prozorec aplikacija=new Prozorec();
+		aplikacija.setVisible(true);
+
+	}
+
+}
---
+++ TelefonskiImenik/src/Prozorec.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+
+public class Prozorec extends JFrame{
+	JLabel l1,l2,l3,l4,l5;
+	JTextField t1,t2,t3,t4;
+	JButton vnes,pregled;
+	JRadioButton rb1,rb2;
+	
+	public Prozorec(){
+		
+		setTitle("Personalen tel.imenik");
+		setSize(400,230);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		
+		
+}}
